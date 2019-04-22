46f4fda566fa027daa3f91ce0c107ec836f45f92
ken <block24block@gmail.com>
2019-04-23 00:24:59 +0800

    windows format vary

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"runtime"
 
 	"github.com/PuerkitoBio/goquery"
@@ -18,19 +19,35 @@ func main() {
 		panic(err)
 	}
 
+	var divs *goquery.Selection
+
+	if runtime.GOOS == "windows" {
+		divs = windowsBody(data)
+	} else {
+		divs = macBody(data)
+	}
+
+	p := &post{}
+	p.parseBody(divs)
+
+	fmt.Println(len(p.paragraphs))
+	fmt.Println(p.String())
+}
+
+func windowsBody(data []byte) *goquery.Selection {
+	r, _ := regexp.Compile(`(?s)<a name="\d+"/>.*?<br/>`)
+	data = r.ReplaceAll(data, []byte(""))
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(data))
 	if err != nil {
 		panic(err)
 	}
+	return doc.Find("div > span").Children()
+}
 
-	p := &post{}
-
-	if runtime.GOOS == "windows" {
-		p.parseBody(doc.Find("body > div").Children())
-	} else {
-		p.parseBody(doc.Find("body").Children())
+func macBody(data []byte) *goquery.Selection {
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(data))
+	if err != nil {
+		panic(err)
 	}
-
-	fmt.Println(len(p.paragraphs))
-	fmt.Println(p.String())
+	return doc.Find("body").Children()
 }
